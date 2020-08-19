6591df18191faaaf91a15704a61b875406f11613
blithe6317 <1160457037@qq.com>
2020-08-19 22:26:40 +0800

    代码分割、Caching

--- src/index.js
+++ src/index.js
@@ -1,10 +1,62 @@
-import _ from "loadsh";
-import "./style.css";
+/**
+ * Output Management
+ */
+
+// import _ from "lodash";
+// import printMe from "./print.js";
+
+// function component() {
+//   const element = document.createElement("div");
+//   const btn = document.createElement("button");
+
+//   element.innerHTML = _.join(["Hello", "webpack"], " ");
+
+//   btn.innerHTML = "Click me and check the console!";
+//   btn.onclick = printMe;
+//   element.appendChild(btn);
+
+//   return element;
+// }
+
+// document.body.appendChild(component());
+
+// 代码分割
+// async function getComponent() {
+//   // return import("lodash")
+//   //   .then(({ default: _ }) => {
+//   //     const element = document.createElement("div");
+//   //     element.innerHTML = _.join(["Hello", "webpack"], " ");
+//   //     return element;
+//   //   })
+//   //   .catch((error) => "An error occurred while loading the component");
+//   const element = document.createElement("div");
+//   const { default: _ } = await import(/*webpackChunkName:"lodash" */ "loadsh");
+//   element.innerHTML = _.join(["Hello", "webpack"], " ");
+//   return element;
+// }
+
+// getComponent().then((component) => {
+//   document.body.appendChild(component);
+// });
+
+/**
+ * Caching
+ */
+
+import _ from "lodash";
+// import printMe from "./print.js";
 
 function component() {
   const element = document.createElement("div");
+  // const btn = document.createElement("button");
+
   element.innerHTML = _.join(["Hello", "webpack"], " ");
-  element.classList.add("hello");
+
+  // btn.innerHTML = "Click me and check the console!";
+  // btn.onclick = printMe;
+  // element.appendChild(btn);
+  // console.log("222");
   return element;
 }
+
 document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,72 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+// module.exports = {
+//   mode: "development",
+//   entry: {
+//     index: "./src/index.js",
+//   },
+//   devtool: "inline-source-map",
+//   devServer: {
+//     contentBase: "./dist",
+//   },
+//   plugins: [
+//     new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+//     new HtmlWebpackPlugin({ title: "Webpack 学习" }),
+//   ],
+//   output: {
+//     filename: "[name].[contenthash].js",
+//     // chunkFilename: "[name].bundle.js",
+//     path: path.resolve(__dirname, "dist"),
+//     // publicPath: "/",
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /\.css$/,
+//         use: ["style-loader", "css-loader"],
+//       },
+//       {
+//         test: /\.(png|svg|jpg|gif)$/,
+//         use: ["file-loader"],
+//       },
+//     ],
+//   },
+// };
+
+/**
+ * Caching
+ */
 
 module.exports = {
-  entry: "./src/index.js",
+  mode: "development",
+  entry: {
+    index: "./src/index.js",
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./dist",
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({ title: "Webpack 学习" }),
+  ],
   output: {
-    filename: "bundle.js",
+    filename: "[name].[contenthash].js",
     path: path.resolve(__dirname, "dist"),
   },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+  optimization: {
+    moduleIds: "hashed",
+    runtimeChunk: "single",
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: "vendors",
+          chunks: "all",
+        },
       },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: ["file-loader"],
-      },
-    ],
+    },
   },
 };
